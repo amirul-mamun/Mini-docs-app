087dd7eee2613f3784e06d2d2feca4963a8e7c59
amirul-mamun <amirulislam21354@gmail.com>
2026-04-20 10:06:02 +0600

    complete prjects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { MdClose } from "react-icons/md";
+import { motion, scale } from "motion/react"
 
-const Card = () => {
+
+const Card = ({data, refrence}) => {
   return (
-    <div>
-        <div className='h-70 w-52 rounded-2xl bg-sky-300' ></div>
 
-    </div>
+      <motion.div
+       drag dragConstraints={refrence}
+       dragElastic={.2} 
+       dragTransition={{bounceStiffness : 100, bounceDamping : 100}}
+       whileDrag={{scale : 1.2}}
+       className='h-65 w-53 relative mix-blend-hue rounded-[20%] bg-zinc-900 text-white py-10 px-8 overflow-hidden' >
+        <FaRegFileAlt/>
+        <p className='text-sm mt-5 font-semibold leading-tight'>{data.desc}</p>
+        
+        <div className="footer absolute bottom-0   w-full  left-0 ">
+             <div className="flex items-center justify-between px-8 py-3  mb-3">
+                 <h5>{data.filesize}</h5>
+                 <span className='h-7 w-7 flex items-center justify-center bg-zinc-600 rounded-full'>
+                  {data.close ? <MdClose/> :<LuDownload size=".8em" color='#fff'/>}
+                 </span>              
+            </div>
+
+            {data.tag.isOpen && (
+            <div className={`tag w-full py-4 flex justify-center items-center ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} `} >
+              <h3 className='text-sm font-semibold '>{data.tag.tagTitle}</h3>
+            </div>
+            ) }
+        </div>
+
+      </motion.div>
+
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+  const ref = useRef(null)
+  const data = [
+    {
+      desc : "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize : ".9mb",
+      close: true,
+      tag : { isOpen : true, tagTitle: "Download Now", tagColor: 'green'},
+    },
+    {
+      desc : "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize : ".9mb",
+      close: true,
+      tag : { isOpen : true, tagTitle: "Download Now", tagColor: 'blue'},
+    },
+    {
+      desc : "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize : ".9mb",
+      close: true,
+      tag : { isOpen : true, tagTitle: "Upload", tagColor: 'green'},
+    },
+  ]
   return (
-    <div> 
-       <Card/>
+    <div>
+       <div ref={ref} className='fixed z-3 left-0 p-5 top-0 w-full h-full flex gap-10 flex-wrap'>
+        {data.map((item, index) =>(
+        <Card data ={item} refrence = {ref} />
+        ))}
+       </div>
     </div>
   )
 }
